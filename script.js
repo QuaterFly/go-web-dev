c216f1a8a6e743cfdf775fa86505bb83847e192a
Solovei Fyodor <frederikusfedr@gmail.com>
2020-09-11 19:52:36 +0300

    Added text for the button

--- script.js
+++ script.js
@@ -3,4 +3,12 @@ let themeButton = document.querySelector('#theme-button')
 themeButton.onclick = function () {
     page.classList.toggle('light-theme');
     page.classList.toggle('dark-theme');
+    console.log(themeButton.textContent)
+    if (document.body.classList.contains('light-theme')) {
+        themeButton.textContent = 'dark-theme'
+    }
+    if (document.body.classList.contains('dark-theme')) {
+        themeButton.textContent = 'light-theme'
+    }
+
 };
